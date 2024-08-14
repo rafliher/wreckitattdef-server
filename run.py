8ae81b098ec4d46312dbaafc5e0d6179a955a184
vps platform wreckit <raflihw1@gmail.com>
2024-08-14 01:11:38 +0000

    development mode

--- run.py
+++ run.py
@@ -3,6 +3,6 @@
 from app import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app, debug=False, host="0.0.0.0", use_reloader=False)
+    socketio.run(app, debug=False, host="0.0.0.0", use_reloader=False, allow_unsafe_werkzeug=True)
     # app.run(debug=False, host="0.0.0.0", use_reloader=False)
     # app.run(debug=True, host="0.0.0.0", use_reloader=False)
