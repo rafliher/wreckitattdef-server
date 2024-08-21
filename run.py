39e99de2346ea0d60bc3248e891a9f5e9f37b529
rafliher <raflihw1@gmail.com>
2024-08-21 08:09:02 +0700

    update server

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 # run.py
-
+import logging
 from app import app, socketio
+
+# Disable Flask logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
     socketio.run(app, debug=False, host="0.0.0.0", use_reloader=False, allow_unsafe_werkzeug=True)
